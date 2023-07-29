aba2761e2aa7b4d52481a1459b832cc31bedbe47
ShahadAldoori <140641553+ShahadAldoori@users.noreply.github.com>
2023-07-29 13:02:33 -0400

    Update DemoDay5.java

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -4,4 +4,7 @@ public class DemoDay5 {
 public static void main(String[] args) {
 	System.out.println("today is sunny");
 }
+	public static void getData() {
+	System.out.println("nice weather");
+}
 }
