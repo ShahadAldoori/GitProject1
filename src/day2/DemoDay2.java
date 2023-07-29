b75f6029de88970192027d9ddab06f3e59c8ee96
aliqa <aliqa@LAPTOP-S2GMTHOI.mynetworksettings.com>
2023-07-29 12:59:56 -0400

    commit

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -1,22 +1,31 @@
 package day2;
 
 public class DemoDay2 {
-public static void main(String[] args) {
-	System.out.println("Test");
-}
-	public static void add(int a,int b) {
-		System.out.println(a+b);
-	
-}
-	public static void subtract(int a,int b) {
-		System.out.println(b-b);
+	public static void main(String[] args) {
+		System.out.println("Test");
 	}
-		public static void add(int a,int b,int c) {
-		System.out.println(a+b+c);
-	
-}
-			public static void add(double a,double b) {
-		System.out.println(a+b);
-	
-}
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+
+	}
+
+	public static void subtract(int a, int b) {
+		System.out.println(b - b);
+	}
+
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+
+	}
+
+	public static void add(double a, double b) {
+		System.out.println(a + b);
+
+	}
+	public static void add(double a, double b,double c) {
+		System.out.println(a + b+c);
+
+	}
+
 }
